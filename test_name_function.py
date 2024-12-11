33fa3496a41ed6381506b047f4780f8eaedb7de4
hbisetto <hbisetto@yahoo.com.br>
2024-12-11 09:42:49 -0300

    add files

---
+++ test_name_function.py
@@ -0,0 +1,12 @@
+from name_function import get_formatted_name
+
+def test_first_last_name():
+    """Nomes como 'Janis Joplin' funcionam?"""
+    formatted_name = get_formatted_name('janis', 'joplin')
+    assert formatted_name == 'Janis Joplin'
+    
+def test_first_last_middle_name():
+    """Nomes como 'Wolfgang Amadeus Mozart' funcionam?"""
+    formatted_name = get_formatted_name(
+        'wolfgang', 'mozart', 'amadeus' )
+    assert formatted_name == "Wolfgang Amadeus Mozart"
